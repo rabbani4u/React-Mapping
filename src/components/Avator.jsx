c14974098e3adbaee065db70ac963d48d3dade29
rabbani4u <rabbani.iu31@gmail.com>
2022-11-24 13:14:43 +0600

    Initialize

---
+++ src/components/Avator.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Avator(props) {
+  return <img className="circle-img" src={props.img} alt="avatar_img" />;
+}
+export default Avator;
---
+++ src/components/Details.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Details(props) {
+  return (
+    <div>
+      <p className="info">{props.detailinfo}</p>
+    </div>
+  );
+}
+export default Details;
